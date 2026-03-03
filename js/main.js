892f09b86818ab20c048a8bac21bc2a3c7136c6e
elbekprogrammer <elbekprogremmer1@gmain.com>
2026-03-04 02:42:28 +0300

    Saytga yangi o'zgartirish kiritildi

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'uz',
+        includedLanguages: 'uz,ru,en'
+    }, 'google_translate_element');
+}
